3d1994c00dac206c162160dafddee39283392c3c
davidisimo040 <david.asandoaiei@yahoo.com>
2020-05-27 16:06:13 +0300

    databaseUPDATE

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const options = {
 const hostname = 'localhost';
 const port = 3000;
 
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://stol:drbirds@stol-8lwdg.mongodb.net/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
+
 const server = https.createServer(options,function(req, res) {
     
     res.setHeader('Access-Control-Allow-Origin', '*');
